b3d093f2bae420430ebd10089af8ade1456437de
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-11-14 01:05:06 -0600

    inital file transfer

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Burger - No Onions');
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Gargantuan', true);
+INSERT INTO burgers (name, devoured) VALUES ('Plain Burger', true);
+INSERT INTO burgers (name) VALUES ('Super Triple BBQ Burger');

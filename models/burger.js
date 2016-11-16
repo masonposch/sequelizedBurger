ba3afa31f3ce4bcf7f130601072f024ae3fa0330
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-11-16 01:31:59 -0600

    begin sequelizing

--- models/burger.js
+++ models/burger.js
@@ -2,25 +2,31 @@
 Here is where you setup a model for how to interface with the database.
 */
 
-var orm = require('../config/orm.js');
+// var orm = require('../config/orm.js');
 
-var burger = {
-	all: function (cb) {
-		orm.all('burgers', function (res) {
-			cb(res);
-		});
-	},
-	// cols and vals are arrays
-	create: function (cols, vals, cb) {
-		orm.create('burgers', cols, vals, function (res) {
-			cb(res);
-		});
-	},
-	update: function (objColVals, condition, cb) {
-		orm.update('burgers', objColVals, condition, function (res) {
-			cb(res);
-		});
-	}
-};
+// var burger = {
+// 	all: function (cb) {
+// 		orm.all('burgers', function (res) {
+// 			cb(res);
+// 		});
+// 	},
+// 	// cols and vals are arrays
+// 	create: function (cols, vals, cb) {
+// 		orm.create('burgers', cols, vals, function (res) {
+// 			cb(res);
+// 		});
+// 	},
+// 	update: function (objColVals, condition, cb) {
+// 		orm.update('burgers', objColVals, condition, function (res) {
+// 			cb(res);
+// 		});
+// 	}
+// };
 
-module.exports = burger;
+// module.exports = burger;
+
+module.exports = function(sequelize, DataTypes) {
+
+	var 
+
+}

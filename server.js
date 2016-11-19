b3e8b9a7783d0706486cd0d63b3e7ec8f602a901
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-11-19 01:56:53 -0600

    working on server.js

--- server.js
+++ server.js
@@ -10,22 +10,89 @@ var methodOverride = require('method-override');
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+// app.use(express.static(process.cwd() + '/public'));
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+// app.use(bodyParser.urlencoded({
+// 	extended: false
+// }));
+// // override with POST having ?_method=DELETE
+// app.use(methodOverride('_method'));
+// var exphbs = require('express-handlebars');
+// app.engine('handlebars', exphbs({
+// 	defaultLayout: 'main'
+// }));
+// app.set('view engine', 'handlebars');
 
-var routes = require('./controllers/burgers_controller.js');
-app.use('/', routes);
+// var routes = require('./controllers/burgers_controller.js');
+// app.use('/', routes);
 
-var port = 3000;
-app.listen(port);
-console.log('App listening on PORT ' + port);
+// var port = 3000;
+// app.listen(port);
+// console.log('App listening on PORT ' + port);
+
+
+var models = require('./models');
+
+var sequelizedConnection = models.sequelize;
+
+sequelizedConnection.query('SET FOREIGN_KEY_CHECKS = 0')
+
+.then(function(){
+	sequelizedConnection.sync({ force: true })
+})
+
+
+
+.then(function(){
+	return models.burger.create(
+	{
+		name: "Bacon Cheeseburger",
+		devoured: True
+	},
+	{
+		name: "Onionizer",
+		devoured: false
+	},
+	{
+		name: "Veggie Burger",
+		devoured: false
+	},
+	{
+		name: "Too Good Burger",
+		devoured: true
+	}
+	)
+	.then(function(){
+		return burger;
+	})
+})
+
+
+app.get('/', function(req, res){
+	res.send(
+		"<h1>Remember your routes!</h1>" +
+		"<h2>/uniform and /employees</h2>"
+	);
+});
+
+
+app.get('/burger', function(req, res){
+	res.json(burger);
+});
+
+
+// listen on port 3000
+app.listen(3000, function(){
+	console.log("Listening on port 3000")
+});
+
+
+
+
+
+
+
+
+
+
+
